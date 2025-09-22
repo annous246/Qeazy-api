1b3572f2d92a17dadf6cf2fa11228d006f531de8
annous246 <anasrabhi0@gmail.com>
2025-09-22 17:04:20 +0100

    changed bash

--- AI_service/run_venv.sh
+++ AI_service/run_venv.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Accept text from argument
-text="$1"
+# # Accept text from argument
+# text="$1"
 
-# Activate venv
-source ./AI_service/AI_summarzier/venv/bin/activate
+# # Activate venv
+# source ./AI_service/AI_summarzier/venv/bin/activate
 
-# Run Python script with argument
-python3 ./AI_service/AI_summarzier/AI_summarizer.py "$text"
+# # Run Python script with argument
+# python3 ./AI_service/AI_summarzier/AI_summarizer.py "$text"
+
+python3 ./AI_service/AI_summarzier/AI_summarizer.py
